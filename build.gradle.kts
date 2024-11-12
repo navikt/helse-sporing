d4089b8efee04274090d395c9ec2601f36d52575
david steinsland <david@davidsteinsland.net>
2024-11-12 20:58:04 +0100

    bumper versjoner

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ val hikariVersion = "5.0.1"
 val kotliqueryVersion = "1.9.0"
 val junitJupiterVersion = "5.11.3"
 val testcontainersVersion = "1.19.5"
-val rapidsAndRiversVersion = "2024111211071731406062.648687519469"
-val tbdLibsVersion = "2024.11.12-11.09-16cf2599"
+val rapidsAndRiversVersion = "2024111220531731441232.6f0a7a6c643b"
+val tbdLibsVersion = "2024.11.12-20.46-e1fd6a25"
 
 plugins {
     kotlin("jvm") version "2.0.21"
