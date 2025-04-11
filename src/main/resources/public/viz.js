31680626f219e7e058ce18e7fe5de2a3c7b2a5fa
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2025-04-11 23:41:31 +0200

    Kanskje dette funker?

--- src/main/resources/public/viz.js
+++ src/main/resources/public/viz.js
@@ -1,10 +1,12 @@
+import { Graphviz } from "https://cdn.jsdelivr.net/npm/@hpcc-js/wasm/dist/graphviz.js";
 
-function fetchJsonAndDisplayGraph(url) {
-    fetch(url)
-        .then(function (response) {
-           return response.text()
-        })
-        .then(function (dot) {
-            const graphviz = d3.select("body").graphviz().renderDot(dot)
-        })
+async function fetchJsonAndDisplayGraph(url) {
+    const response = await fetch(url)
+    const dot = await response.text()
+
+    const graphviz = await Graphviz.load();
+    const div = document.getElementById("placeholder");
+    div.innerHTML = graphviz.layout(dot, "svg", "dot")
 }
+
+export default fetchJsonAndDisplayGraph
