0d07264d567f10316a76e417a7f51a0104b50794
Amalie Mansåker <amalie.erdal.mansaker@nav.no>
2025-09-22 10:06:19 +0200

    Bumper tbd-libs
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ val hikariCPVersion = "6.3.0"
 val kotliqueryVersion = "1.9.0"
 val junitJupiterVersion = "5.12.1"
 val rapidsAndRiversVersion = "2025081612341755340488.ff2c2d01e04f"
-val tbdLibsVersion = "2025.09.19-13.31-61342e73"
+val tbdLibsVersion = "2025.09.19-15.24-1a9c113f"
 val postgresqlVersion = "42.7.7"
 
 plugins {
