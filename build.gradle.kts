761be2c1f941f9ee24028ca8eb74c117f6308633
david steinsland <david@davidsteinsland.net>
2025-11-04 10:28:54 +0100

    bumper kotlin jvm, tbd libs og r&r

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,12 @@ val flywayVersion = "11.5.0"
 val hikariCPVersion = "6.3.0"
 val kotliqueryVersion = "1.9.0"
 val junitJupiterVersion = "5.12.1"
-val rapidsAndRiversVersion = "2025092210081758528498.1d4b108f2c61"
-val tbdLibsVersion = "2025.09.19-15.24-1a9c113f"
+val rapidsAndRiversVersion = "2025110410191762247980.5e0592e08597"
+val tbdLibsVersion = "2025.11.04-10.12-3a167132"
 val postgresqlVersion = "42.7.7"
 
 plugins {
-    kotlin("jvm") version "2.2.10"
+    kotlin("jvm") version "2.2.21"
 }
 
 repositories {
