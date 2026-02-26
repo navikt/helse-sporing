d83298e6c090603aadb016e7bd8365dceb00d6c0
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2026-02-26 14:13:00 +0100

    Bump Kotlin JVM -> 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ val tbdLibsVersion = "2026.01.22-09.16-1d3f6039"
 val postgresqlVersion = "42.7.7"
 
 plugins {
-    kotlin("jvm") version "2.2.21"
+    kotlin("jvm") version "2.3.0"
 }
 
 repositories {
